5315bc427f1c55feac21b477d617567adc23a065
imtiaz.rahi <imtiaz.rahi@naztech.us.com>
2019-08-14 22:06:53 +0600

    Solution updated based on StefanPochmann submission

---
+++ Electronic Station/Roman Numerals/best-1.py
@@ -0,0 +1,16 @@
+# https://py.checkio.org/mission/roman-numerals/publications/StefanPochmann/python-3/short-and-clear
+def checkio(n):
+    result = ''
+    for arabic, roman in zip((1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1),
+                             'M     CM   D    CD   C    XC  L   XL  X   IX V  IV I'.split()):
+        result += n // arabic * roman
+        n %= arabic
+    return result
+
+
+if __name__ == '__main__':
+    assert checkio(6) == 'VI', '6'
+    assert checkio(76) == 'LXXVI', '76'
+    assert checkio(499) == 'CDXCIX', '499'
+    assert checkio(3888) == 'MMMDCCCLXXXVIII', '3888'
+
--- Electronic Station/Roman Numerals/mission.py
+++ Electronic Station/Roman Numerals/mission.py
@@ -1,16 +1,15 @@
+# https://py.checkio.org/mission/roman-numerals/publications/StefanPochmann/python-3/short-and-clear/
 def checkio(data):
     ints = (1000, 900,  500, 400, 100,  90,   50,  40,   10,   9,    5,   4,    1)
     nums = ('M',  'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
     result = []
-    for i in range(len(ints)):
-        count = int(data / ints[i])
-        result.append(nums[i] * count)
-        data -= ints[i] * count
+    for arabic, roman in zip(ints, nums):
+        result.append(data // arabic * roman)
+        data %= arabic
     return ''.join(result)
 
 
 if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert checkio(6) == 'VI', '6'
     assert checkio(76) == 'LXXVI', '76'
     assert checkio(499) == 'CDXCIX', '499'
