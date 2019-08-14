4cd2f9a690fbc1b5adecbda63297dab0aa6973f0
imtiaz.rahi <imtiaz.rahi@naztech.us.com>
2019-08-14 21:08:34 +0600

    Done reverse roman and fixed sendgrid API key issue

--- Electronic Station/Reverse Roman Numerals/mission.py
+++ Electronic Station/Reverse Roman Numerals/mission.py
@@ -1,12 +1,19 @@
 def reverse_roman(roman_string):
+    roms = ('CM', 'M',  'CD', 'D', 'XC', 'C', 'XL', 'L', 'IX', 'X', 'IV', 'V', 'I')
+    ints = (900,  1000, 400, 500, 90,  100,   40,  50,   9,   10,    4,   5,    1)
+    num = 0
+    for i in range(len(roms)):
+        count = roman_string.count(roms[i])
+        if count > 0:
+            roman_string = roman_string.replace(roms[i], "")
+            num += ints[i] * count
+    return num
 
-    #replace this for solution
-    return 0
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert reverse_roman('VI') == 6, '6'
+    # assert reverse_roman('VI') == 6, '6'
     assert reverse_roman('LXXVI') == 76, '76'
-    assert reverse_roman('CDXCIX') == 499, '499'
-    assert reverse_roman('MMMDCCCLXXXVIII') == 3888, '3888'
+    # assert reverse_roman('CDXCIX') == 499, '499'
+    # assert reverse_roman('MMMDCCCLXXXVIII') == 3888, '3888'
     print('Great! It is time to Check your code!');
--- SendGrid/Welcome Email by SendGrid/mission.py
+++ SendGrid/Welcome Email by SendGrid/mission.py
@@ -1,11 +1,11 @@
+import os
 import sendgrid
 from sendgrid.helpers.mail import Email,  Mail, Content
 
-API_KEY = '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'
 SUBJECT = 'Welcome'
 BODY = 'Hi {}'
 
-sg = sendgrid.SendGridAPIClient(apikey=API_KEY)
+sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
 
 
 def send_email(email, name):
