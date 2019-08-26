501cd6130e2e9a70030a86cf54ac6988d837fb3e
imtiaz.rahi <imtiaz.rahi@naztech.us.com>
2019-08-26 20:34:29 +0600

    done another mission

--- Mine/Caesar Cipher (encryptor)/mission.py
+++ Mine/Caesar Cipher (encryptor)/mission.py
@@ -1,12 +1,12 @@
 def to_encrypt(text, delta):
-    #replace this for solution
-    return text
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    return "".join([alpha[(alpha.index(ch) + delta)%26] if ch != " " else " " for ch in text])
+
 
 if __name__ == '__main__':
     print("Example:")
     print(to_encrypt('abc', 10))
 
-    #These "asserts" using only for self-checking and not necessary for auto-testing
     assert to_encrypt("a b c", 3) == "d e f"
     assert to_encrypt("a b c", -3) == "x y z"
     assert to_encrypt("simple text", 16) == "iycfbu junj"
