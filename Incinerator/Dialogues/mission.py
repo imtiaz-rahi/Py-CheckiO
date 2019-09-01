d9d4b6dcd9c628ad3e193ef50140fc0d321a1845
imtiaz.rahi <imtiaz.rahi@naztech.us.com>
2019-09-01 18:59:19 +0600

    Completed Incinerator -> Dialogue

--- Incinerator/Dialogues/mission.py
+++ Incinerator/Dialogues/mission.py
@@ -1,17 +1,56 @@
 VOWELS = "aeiou"
 
+
 class Chat:
+    def __init__(self):
+        self.messages = []
+
+    def connect_human(self, human):
+        human.chat = self
+
+    def connect_robot(self, bot):
+        bot.chat = self
+
+    def show_human_dialogue(self):
+        return self.show_dialogue(True)
+
+    def show_robot_dialogue(self):
+        return self.show_dialogue(False)
+
+    def send(self, user, msg):
+        self.messages.append([user.name, msg])
+
+    def show_dialogue(self, is_human):
+        result = ""
+        for it in self.messages:
+            result += f'{it[0]} said: {self.translate(it[1], is_human)}\n'
+        return result[:len(result) - 1]
+
+    def translate(self, val: str, is_human: bool) -> str:
+        if is_human: return val
+        is_vowel = lambda x: '0' if x in VOWELS else '1'
+        return ''.join([is_vowel(ch) for ch in val])
+
+
+class Participant:
+    def __init__(self, name):
+        self.name = name
+        self.chat: Chat = None
+
+    def send(self, msg):
+        self.chat.send(self, msg)
+
+
+class Human(Participant):
     pass
 
-class Human:
-    pass
 
-class Robot:
+class Robot(Participant):
     pass
 
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
 
     chat = Chat()
     karl = Human("Karl")
