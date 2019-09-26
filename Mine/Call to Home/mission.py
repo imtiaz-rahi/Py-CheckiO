09771db057f299a35e58ef3e71b3fe2af0ff423a
imtiaz-rahi <imtiaz.rahi@gmail.com>
2019-09-26 15:15:58 +0600

    Completed call to home
    Signed-off-by: imtiaz-rahi <imtiaz.rahi@gmail.com>

--- Mine/Call to Home/mission.py
+++ Mine/Call to Home/mission.py
@@ -1,9 +1,16 @@
+from math import ceil
+
+
 def total_cost(calls):
-    return 0
+    bill = lambda d: 100 + (d - 100) * 2 if d > 100 else d
+    # List of call duration in minutes; [('2014-02-05', 1), ('2014-02-05', 2), ('2014-02-06', 3)]
+    data = [(dt, ceil(int(dur) / 60)) for dt, tm, dur in (call.split() for call in calls)]
+    # Sum each day call durations and calculate bill
+    data = {uk: bill(sum(v for k, v in data if k == uk)) for uk in set(k for k, _ in data)}
+    return sum(data.values())
 
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
     assert total_cost(("2014-01-01 01:12:13 181",
                        "2014-01-02 20:11:10 600",
                        "2014-01-03 01:12:13 6009",
@@ -16,3 +23,12 @@ if __name__ == '__main__':
                        "2014-02-05 02:00:00 60",
                        "2014-02-05 03:00:00 60",
                        "2014-02-05 04:00:00 6000")) == 106, "Precise calls"
+    assert total_cost(["2057-01-01 00:00:00 7200",
+                       "2057-01-01 03:00:00 7200",
+                       "2057-01-01 06:00:00 7200",
+                       "2057-01-01 09:00:00 7200",
+                       "2057-01-01 12:00:00 7200",
+                       "2057-01-01 15:00:00 7200",
+                       "2057-01-01 18:00:00 7200",
+                       "2057-01-01 21:00:00 7200",
+                       "2057-01-01 23:10:00 7200"]) == 2060, "Edge test 5/5"
